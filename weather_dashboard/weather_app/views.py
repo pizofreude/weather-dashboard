af99811556c03394eb93fcfdd96200dd9f45c29e
Pizofreude <108355948+pizofreude@users.noreply.github.com>
2023-08-23 00:22:28 +0800

    weather_app/views.py: Update city

--- weather_dashboard/weather_app/views.py
+++ weather_dashboard/weather_app/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
-# Create your views here.
+# # Create your views here.
+# views.py
+from django.shortcuts import render
+from .weather_fetcher import fetch_weather_data
+
+def weather_dashboard(request):
+    # Replace 'Hamburg,DE' with the city you want to display weather data for.
+    weather_data = fetch_weather_data('Hamburg,DE')
+
+    context = {
+        'weather_data': weather_data,
+    }
+
+    return render(request, 'weather_app/dashboard.html', context)
+
