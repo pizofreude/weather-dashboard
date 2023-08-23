ac59e1a9605bc0e2cdf9884b4b00f38d4fbb97e7
Pizofreude <108355948+pizofreude@users.noreply.github.com>
2023-08-23 18:12:14 +0800

    Register and load static tag

--- weather_dashboard/weather_dashboard/settings.py
+++ weather_dashboard/weather_dashboard/settings.py
@@ -117,8 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [BASE_DIR / 'static']
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = BASE_DIR / 'static_root'
