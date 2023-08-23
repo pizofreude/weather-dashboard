1ec7fbd3d1f2ecf7db811d4a8ca356c1db3974da
Pizofreude <108355948+pizofreude@users.noreply.github.com>
2023-08-23 18:30:31 +0800

    weather_fetcher.py: Fix hardcoded city function

--- weather_dashboard/weather_app/weather_fetcher.py
+++ weather_dashboard/weather_app/weather_fetcher.py
@@ -5,7 +5,8 @@ BASE_URL = 'https://api.openweathermap.org/data/2.5/weather'
 
 def fetch_weather_data(city):
     params = {
-        'q': 'Hamburg,DE',  # 'Hamburg' is the city name, and 'DE' is the country code for Germany.
+        # 'q': 'Hamburg,DE',  # 'Hamburg' is the city name, and 'DE' is the country code for Germany.
+        'q': f'{city}',  # Use the 'city' parameter passed to the function.
         'appid': config.OPENWEATHERMAP_API_KEY,  # Access the API key from the 'config' module.
         'units': 'metric'
     }
