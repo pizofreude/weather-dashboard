d1210f67ef4a155a59412a85043b5d1fdca8b365
Pizofreude <108355948+pizofreude@users.noreply.github.com>
2023-08-25 18:28:48 +0800

    views.py: Fix error handling display message

--- weather_dashboard/weather_app/views.py
+++ weather_dashboard/weather_app/views.py
@@ -3,22 +3,29 @@
 # # Create your views here.
 # views.py
 from django.shortcuts import render
-from .weather_fetcher import fetch_weather_data
 from requests.exceptions import RequestException    # Handling errors
+from .weather_fetcher import fetch_weather_data
 
 
 def weather_dashboard(request):
+    error_message = None    # Default error message is None
     # Replace 'Hamburg,DE' with the city you want to display weather data for.
     city = request.GET.get('city', 'Hamburg,DE')  # Default to Hamburg, Germany
-    error_message = None    # Default error message is None
 
     try:
         # Attempt to fetch weather data
         weather_data = fetch_weather_data(city)
 
+        if 'main' not in weather_data or 'temp' not in weather_data['main']:
+            raise KeyError("Invalid weather data format")
+
     except RequestException as e:
         # Handle API request error
         error_message = f"Error fetching weather data: {str(e)}"
+        weather_data = None
+
+    except KeyError as e:
+        error_message = f"Error processing weather data: {str(e)}"
         weather_data = None
 
     context = {
