af95d85832dcfbd23bf71a788fe03eecc18f3073
Pizofreude <108355948+pizofreude@users.noreply.github.com>
2023-08-25 14:45:31 +0800

    views.py: Add error handling - try-except block on API request - set default value to None - setup alert

--- weather_dashboard/weather_app/views.py
+++ weather_dashboard/weather_app/views.py
@@ -4,15 +4,24 @@
 # views.py
 from django.shortcuts import render
 from .weather_fetcher import fetch_weather_data
+from requests.exceptions import RequestException    # Handling errors
+
 
 def weather_dashboard(request):
-    # Replace 'Hamburg,DE' with the city you want to display weather data for.
-    city = request.GET.get('city', 'Hamburg,DE')  # Default to Hamburg, Germany
-    weather_data = fetch_weather_data(city)
+    try:
+        # Replace 'Hamburg,DE' with the city you want to display weather data for.
+        city = request.GET.get('city', 'Hamburg,DE')  # Default to Hamburg, Germany
+        weather_data = fetch_weather_data(city)
+
+    except RequestException as e:
+        # Handle API request error
+        error_message = f"Error fetching weather data: {str(e)}"
+        weather_data = None
 
     context = {
         'weather_data': weather_data,
         'city': city, # Pass the 'city' variable to the template.
+        'error_message': error_message if 'error_message' in locals() else None,
     }
 
     return render(request, 'weather_app/dashboard.html', context)
