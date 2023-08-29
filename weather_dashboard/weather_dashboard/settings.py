7cd8b045b15d2ba071f772d476fbec74c5c08c56
Pizofreude <108355948+pizofreude@users.noreply.github.com>
2023-08-29 17:58:41 +0800

    weather_dashboard/weather_dashboard/settings.py: - extract sensitive info and place in config.py - add config.py to .gitignore

--- weather_dashboard/weather_dashboard/settings.py
+++ weather_dashboard/weather_dashboard/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from .config import MY_SECRET_KEY
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g4b#^2l&gkxnftqt#^nmr&ai_j)2jo_#z0*+wmb*)udkrolv3m'
+SECRET_KEY = MY_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
